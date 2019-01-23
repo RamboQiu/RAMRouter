e327f2f91bbbaee4612bd2d93a290a7dc027008c
裘俊云 <hzqiujunyun@corp.netease.com>
2019-01-23 16:31:02 +0800

    添加移除controller的类方法

--- RAMRouter/RAMController/UIViewController+RAMUtils.h
+++ RAMRouter/RAMController/UIViewController+RAMUtils.h
@@ -7,6 +7,14 @@
 
 #import <UIKit/UIKit.h>
 
+/*!
+ *  将Controller从tree中删除，因为dismissviewcontroller接口一定是异步的。。。所以没办法，必须提供一个异步回调接口！
+ *  如果不是present controller从tree中删除，那么这个回调就同步调用
+ *
+ *  @param 这个Controller是否从Controller tree中删除
+ */
+typedef void (^RemoveControllerFromTreeCallback)(BOOL);
+
 @interface UIViewController (RAMUtils)
 + (UIViewController *)ram_applicationRootViewController;
 /*!
@@ -36,6 +44,28 @@
  *  @return navigation controller
  */
 - (UINavigationController *)ram_innerMostNavigationControllerWithNavigationBarHidden:(BOOL)bHidden;
+
+/*!
+ *  将self从Controller tree中删除
+ *  因为dismiss view controller一定是异步，在dismiss没有complete之前同步在presente一个controller，会报错，所以通过RemoveControllerFromTreeCallback来返回结果。如果参数为YES，表示可以删除，否则无法删除。
+ *
+ *  @warning：有些清楚无法删除，譬如：self是tabbarcontroller的一个child，此时cb回调参数为NO
+ 
+ *  如果没有从Controller tree中删除，那么不会对原来Controller tree有任何影响
+ *
+ *  @warning：考虑HTContainerViewController，有可能删除的是self被包裹的ContainerViewController
+ *
+ *
+ *  @param cb 删除之后的回调
+ */
+- (void)ram_removeFromControllerTree:(RemoveControllerFromTreeCallback)cb;
+
+/*!
+ * 业务中经常碰到需要在某些场景中，在当前controller中触发了某种业务之后就不能回退到前一个页面了
+ * 例如：某一个表单跨三个页面填写，最后一个页面提交，提交之后进入一个成功页面，这个时候就需要抽掉前面的三个页面
+ *
+ */
+- (void)ram_removeByUrlPaths:(NSArray<NSString *> *)urlPaths callback:(RemoveControllerFromTreeCallback)cb;
 
 /*!
  *  使用了RAMContainerViewController或者RAMControllerRouter的ViewController可以使用ram_back来实现后退页面的统一功能
--- RAMRouter/RAMController/UIViewController+RAMUtils.m
+++ RAMRouter/RAMController/UIViewController+RAMUtils.m
@@ -7,6 +7,8 @@
 
 #import "UIViewController+RAMUtils.h"
 #import "RAMContainerViewController.h"
+#import "RAMRouterConfig.h"
+#import "RAMRouter.h"
 
 @implementation UIViewController (RAMUtils)
 
@@ -22,6 +24,19 @@
     }
     
     return topController;
+}
+
+- (BOOL)ram_isControllerInPresentedControllerChain {
+    UIViewController *topController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    
+    while (topController) {
+        if (self == topController)
+            return YES;
+        
+        topController = topController.presentedViewController;
+    }
+    
+    return NO;
 }
 
 - (UINavigationController *)ram_innerMostNavigationController {
@@ -59,6 +74,127 @@
     }
     
     return foudController;
+}
+
+- (void)ram_removeFromPresentedControllerChain:(RemoveControllerFromTreeCallback)cb {
+    UIViewController *presentingController = self.presentingViewController;
+    UIViewController *presentedController = self.presentedViewController;
+    if (presentingController == nil){
+        [self dismissViewControllerAnimated:NO completion:^{
+            cb(YES);
+        }];
+        return;
+    }
+    
+    if (self.presentedViewController) {
+        [self dismissViewControllerAnimated:NO completion:^{
+            [presentingController dismissViewControllerAnimated:NO completion:^{
+                if (presentedController)
+                    [presentingController presentViewController:presentedController animated:NO completion:^{
+                        cb(YES);
+                    }];
+            }];
+        }];
+    } else {
+        [presentingController dismissViewControllerAnimated:NO completion:^{
+            if (presentedController)
+                [presentingController presentViewController:presentedController animated:NO completion:^{
+                    cb(YES);
+                }];
+        }];
+    }
+}
+
+- (void)ram_removeFromNavigationController {
+    NSAssert([self.navigationController.viewControllers indexOfObject:self] != NSNotFound, @"controller is not in navigationcontroller");
+    
+    UINavigationController *navigationController = self.navigationController;
+    NSMutableArray *vcs = [[navigationController viewControllers] mutableCopy];
+    [vcs removeObject:self];
+    [navigationController setViewControllers:vcs animated:NO];
+}
+
+/*
+ ==========================================================
+ 关于问题：将一个ViewController显示出来，先从原来的tree中删除，然后放在当前显示的tree上！
+ 
+ 当ViewController由container包裹，
+ 1. 如果container的innerNavigationController的childViewController个数等于1，那么等价于删除ContainerViewController
+ 2. 如果container的innerNavigationController的childViewController个数大于1，无法处理，直接报错
+ 
+ ---------------
+ 
+ 1. 当viewcontroller在另外一个navigation controller中：
+ 1. 如果navigation controller的child view controller个数大于1， 那么直接删除view controller
+ 2. 如果navigation controller的child view controller个数等于1
+ 当navigation controller在presented 链上，那么从链上删除这个vc
+ 否则报错
+ 
+ 2. 如果viewcontroller在presented 链上，那么直接从链上删除（如果是根，dismiss其他vc）
+ 
+ 3. 其他所有情况均无法删除
+ */
+- (void)ram_removeFromControllerTree:(RemoveControllerFromTreeCallback)cb {
+    RAMContainerViewController *containerViewController;
+    if ([self conformsToProtocol:@protocol(RAMContainerViewControllerProtocol)]){
+        containerViewController = [(id<RAMContainerViewControllerProtocol>)self containerController];
+    }
+    
+    if (containerViewController) {
+        if (containerViewController.rootNavigationController.viewControllers.count == 1){
+            return [containerViewController ram_removeFromControllerTree:cb];
+        } else {
+            return cb(NO);
+        }
+    }
+    
+    if ([self ram_isControllerInPresentedControllerChain]){
+        return [self ram_removeFromPresentedControllerChain:cb];
+    }
+    
+    if ([self.parentViewController isKindOfClass:UINavigationController.class]){
+        UINavigationController *parentNavigationController = (UINavigationController*)self.parentViewController;
+        if (parentNavigationController.childViewControllers.count > 1){
+            [self ram_removeFromNavigationController];
+            return cb(YES);
+        }
+        else if (parentNavigationController.childViewControllers.count == 1 &&
+                 [parentNavigationController ram_isControllerInPresentedControllerChain]){
+            return [parentNavigationController ram_removeFromPresentedControllerChain:cb];
+        }
+        
+        return cb(NO);
+    }
+    
+    return cb(NO);
+}
+
+- (void)ram_removeByUrlPaths:(NSArray<NSString *> *)urlPaths callback:(RemoveControllerFromTreeCallback)cb {
+    if ([self conformsToProtocol:@protocol(RAMContainerViewControllerProtocol)]){
+        UINavigationController *containerNavigationController = [(id<RAMContainerViewControllerProtocol>)self containerController].navigationController;
+        NSArray *containerArr = containerNavigationController.viewControllers;
+        NSMutableArray *removeList = [NSMutableArray array];
+        for (NSInteger i = 0; i < containerArr.count; i ++) {
+            id obj = [containerArr objectAtIndex:i];
+            if (![obj isKindOfClass:[RAMContainerViewController class]]) {
+                continue;
+            }
+            RAMContainerViewController *privousVC = (RAMContainerViewController *)obj;
+            UIViewController *firstObject = privousVC.rootNavigationController.viewControllers.firstObject;
+            if ([firstObject.class conformsToProtocol:@protocol(RAMRouteTargetProtocol)]) {
+                RAMRouterConfig *config = (RAMRouterConfig*)[firstObject.class configureRouter];
+                for (NSString *url in config.urls) {
+                    if ([urlPaths containsObject:url]) {
+                        [removeList addObject:privousVC];
+                    }
+                }
+            }
+        }
+        UINavigationController *navigationController = self.navigationController;
+        NSMutableArray *vcs = [[navigationController viewControllers] mutableCopy];
+        [vcs removeObjectsInArray:removeList];
+        [navigationController setViewControllers:vcs animated:NO];
+    }
 }
 
 - (void)ram_back {
